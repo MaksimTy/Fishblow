6777cad8f8dc5e21bcda562887cf08eee71a0a0e
Maksim Tymkovskiy <maksimvt@gmail.com>
2020-01-28 10:56:46 +0300

    AquariumTest.java corrected

--- src/test/java/model/AquariumTest.java
+++ src/test/java/model/AquariumTest.java
@@ -14,33 +14,47 @@ import static org.junit.Assert.*;
  */
 public class AquariumTest {
 
-    private final Map<int[], Integer> terms = new HashMap<>();
+    private final Map<int[], Integer> successSet = new HashMap<>();
+    private final Map<int[], Integer> invalidValuesSet = new HashMap<>();
 
     @Before
     public void setUp() {
-        terms.put(new int[]{0, 0, 0, 0, 0}, 0);
-        terms.put(new int[]{6, 6, 6, 6, 1}, 0);
-        terms.put(new int[]{0, 0, 10, 0, 0}, 0);
+        successSet.put(new int[]{0, 0, 0, 0, 0}, 0);
+        successSet.put(new int[]{6, 6, 6, 6, 1}, 0);
+        successSet.put(new int[]{0, 0, 10, 0, 0}, 0);
 
-        terms.put(new int[]{0, 0, 0, 10, 1}, 0);
-        terms.put(new int[]{10, 0, 0, 0, 10}, 30);
-        terms.put(new int[]{4, 2, 3, 2, 5, 0, 1, 3}, 10);
-        terms.put(new int[]{-4, 2, 3, 2, 5, 0, 1, 3}, -1);
+        successSet.put(new int[]{0, 0, 0, 10, 1}, 0);
+        successSet.put(new int[]{10, 0, 0, 0, 10}, 30);
+        successSet.put(new int[]{4, 2, 3, 2, 5, 0, 1, 3}, 10);
+        invalidValuesSet.put(new int[]{-4, 2, 3, 2, 5, 0, 1, 3}, -1);
 
-        terms.put(new int[Terrain.getN() + 1], -1);
-        terms.put(new int[]{Terrain.getN() + 1}, -1);
-        terms.put(new int[]{0, Terrain.getN()}, 0);
+        invalidValuesSet.put(new int[Terrain.getN() + 1], -1);
+        invalidValuesSet.put(new int[]{Terrain.getN() + 1}, -1);
+        successSet.put(new int[]{0, Terrain.getN()}, 0);
 
     }
 
     @After
     public void tearDown() {
-        terms.clear();
+        successSet.clear();
+        invalidValuesSet.clear();
     }
+
+    @Test(expected = InvalidValuesException.class)
+    public void TestGetWaterWithInvalidValuesException() throws InvalidValuesException {
+        for (Map.Entry<int[], Integer> entry : invalidValuesSet.entrySet()
+        ) {
+            final int[] test = entry.getKey();
+            final Integer expecteds = entry.getValue();
+            final Integer actual = new Aquarium(test).getWater();
+            assertEquals(expecteds, actual);
+        }
+    }
+
 
     @Test
     public void TestGetWater() throws InvalidValuesException {
-        for (Map.Entry<int[], Integer> entry : terms.entrySet()
+        for (Map.Entry<int[], Integer> entry : successSet.entrySet()
         ) {
             final int[] test = entry.getKey();
             final Integer expecteds = entry.getValue();
